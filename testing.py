5d6d4b0bfbd6a5c8650a836d1e4c24af1793b3eb
CharalamposF <75033654+CharalamposF@users.noreply.github.com>
2024-04-11 18:07:26 +0300

    add all hyperparameters and methods to the feature extraction

--- testing.py
+++ testing.py
@@ -34,13 +34,20 @@ def test_modular(data_dir):
     # slots are [LR, KNN, SVM, MLP, CNN, LSTM]
     models_used = [1, 0, 0, 0, 0, 0]
     # This is the modular classifier training stage
-    results_df = test_classifier_mod(k_values_mfcc, models_used)
+    results_df = test_classifier_mod(k_values_mfcc, models_used=models_used)
 
     return results_df
 
 
 # Temporary feature extraction function
-def test_feat_extr(data, k_values_mfcc):
+def test_feat_extr(data, k_values_mfcc=None, k_values_frame=None, k_values_segment=None):
+    if k_values_mfcc is None:
+        k_values_mfcc = [1]
+    if k_values_frame is None:
+        k_values_frame = [-1]
+    if k_values_segment is None:
+        k_values_segment = [-1]
+
     # Initialize the LabelEncoder
     le = LabelEncoder()
 
@@ -48,42 +55,130 @@ def test_feat_extr(data, k_values_mfcc):
     for k_mfcc in k_values_mfcc:
         n_mfcc = 14 * k_mfcc
 
-        # Name of the directory and file where the features will be saved
-        features_folder = "extracted_features/feat_extr_simple"
+        # Feature Extraction Initialization Function with only one method and only one hyperparameter
+        if k_values_frame == [-1]:
+            # Name of the directory and file where the features will be saved
+            features_folder = "extracted_features/feat_extr_simple"
 
-        # Check if the directory exists, if not, create it
-        if not os.path.exists(features_folder):
-            os.makedirs(features_folder)
+            # Check if the directory exists, if not, create it
+            if not os.path.exists(features_folder):
+                os.makedirs(features_folder)
 
-        feature_filename_target = "extracted_features_" + str(k_mfcc) + ".npy"
-        label_filename_target = "extracted_labels_" + str(k_mfcc) + ".npy"
-        feature_filename = os.path.join(features_folder, feature_filename_target)
-        label_filename = os.path.join(features_folder, label_filename_target)
+            feature_filename_target = "extracted_features_" + str(k_mfcc) + ".npy"
+            label_filename_target = "extracted_labels_" + str(k_mfcc) + ".npy"
+            feature_filename = os.path.join(features_folder, feature_filename_target)
+            label_filename = os.path.join(features_folder, label_filename_target)
 
-        successful_indices = []
-        features_list = []
-        # Check if the file doesn't exist
-        if not os.path.exists(feature_filename):
-            # Modified part to extract features and simultaneously filter labels
-            for idx, row in data.iterrows():
-                feat = feat_extr.extract_features_simple(data_dir, row.participantid, row.submissionid, n_mfcc)
-                if feat is not False:
-                    features_list.append(feat)
-                    successful_indices.append(idx)
-            features = np.array(features_list)
+            successful_indices = []
+            features_list = []
+            # Check if the file doesn't exist
+            if not os.path.exists(feature_filename):
+                # Modified part to extract features and simultaneously filter labels
+                for idx, row in data.iterrows():
+                    feat = feat_extr.extract_features_simple(data_dir, row.participantid, row.submissionid, n_mfcc)
+                    if feat is not False:
+                        features_list.append(feat)
+                        successful_indices.append(idx)
+                features = np.array(features_list)
 
-            # Filter labels based on successful feature extraction
-            labels = np.array(data.loc[successful_indices, 'covid_status'])
+                # Filter labels based on successful feature extraction
+                labels = np.array(data.loc[successful_indices, 'covid_status'])
 
-            # Convert labels to a consistent numerical format
-            labels = le.fit_transform(labels)
+                # Convert labels to a consistent numerical format
+                labels = le.fit_transform(labels)
 
-            np.save(feature_filename, features)
-            np.save(label_filename, labels)
+                np.save(feature_filename, features)
+                np.save(label_filename, labels)
+        else:
+            for k_frame in k_values_frame:
+                frame_size = 2 ** k_frame
+                hop_length = frame_size // 2  # 50% overlap
+
+                # Feature Extraction Initialization Function with all five methods and only two hyperparameters
+                if k_values_segment == [-1]:
+                    # Name of the directory and file where the features will be saved
+                    features_folder = "extracted_features/feat_extr"
+
+                    # Check if the directory exists, if not, create it
+                    if not os.path.exists(features_folder):
+                        os.makedirs(features_folder)
+
+                    feature_filename_target = "extracted_features_" + str(k_mfcc) + str(k_frame) + ".npy"
+                    label_filename_target = "extracted_labels_" + str(k_mfcc) + str(k_frame) + ".npy"
+                    feature_filename = os.path.join(features_folder, feature_filename_target)
+                    label_filename = os.path.join(features_folder, label_filename_target)
+
+                    successful_indices = []
+                    features_list = []
+                    # Check if the file doesn't exist
+                    if not os.path.exists(feature_filename):
+                        # Modified part to extract features and simultaneously filter labels
+                        for idx, row in data.iterrows():
+                            feat = feat_extr.extract_features(data_dir, row.participantid, row.submissionid, n_mfcc, frame_size, hop_length)
+                            if feat is not False:
+                                features_list.append(feat)
+                                successful_indices.append(idx)
+                        features = np.array(features_list)
+
+                        # Filter labels based on successful feature extraction
+                        labels = np.array(data.loc[successful_indices, 'covid_status'])
+
+                        # Convert labels to a consistent numerical format
+                        labels = le.fit_transform(labels)
+
+                        np.save(feature_filename, features)
+                        np.save(label_filename, labels)
+                else:
+                    for k_segment in k_values_segment:
+                        n_segments = 10 * k_segment
+
+                        # Feature Extraction Initialization Function with all five methods and all three hyperparameters
+
+                        # Name of the directory and file where the features will be saved
+                        features_folder = "extracted_features/feat_extr_with_segm"
+
+                        # Check if the directory exists, if not, create it
+                        if not os.path.exists(features_folder):
+                            os.makedirs(features_folder)
+
+                        feature_filename_target = "extracted_features_" + str(k_mfcc) + str(k_frame) + str(k_segment) + ".npy"
+                        label_filename_target = "extracted_labels_" + str(k_mfcc) + str(k_frame) + str(k_segment) + ".npy"
+                        feature_filename = os.path.join(features_folder, feature_filename_target)
+                        label_filename = os.path.join(features_folder, label_filename_target)
+
+                        successful_indices = []
+                        features_list = []
+                        # Check if the file doesn't exist
+                        if not os.path.exists(feature_filename):
+                            # Modified part to extract features and simultaneously filter labels
+                            for idx, row in data.iterrows():
+                                feat = feat_extr.extract_features_with_segments(data_dir, row.participantid, row.submissionid, n_mfcc, frame_size, hop_length, n_segments)
+                                if feat is not False:
+                                    features_list.append(feat)
+                                    successful_indices.append(idx)
+                            features = np.array(features_list)
+
+                            # Filter labels based on successful feature extraction
+                            labels = np.array(data.loc[successful_indices, 'covid_status'])
+
+                            # Convert labels to a consistent numerical format
+                            labels = le.fit_transform(labels)
+
+                            np.save(feature_filename, features)
+                            np.save(label_filename, labels)
 
 
 # Temporary classifier function
-def test_classifier_mod(k_values_mfcc, models_used):
+def test_classifier_mod(k_values_mfcc, k_values_frame=None, k_values_segment=None, models_used=None):
+    if k_values_mfcc is None:
+        k_values_mfcc = [1]
+    if k_values_frame is None:
+        k_values_frame = [-1]
+    if k_values_segment is None:
+        k_values_segment = [-1]
+    if models_used is None:
+        models_used = [1, 0, 0, 0, 0, 0]
+
     # Initialize list for storing results
     results = []
 
@@ -91,27 +186,86 @@ def test_classifier_mod(k_values_mfcc, models_used):
     for k_mfcc in k_values_mfcc:
         n_mfcc = 14 * k_mfcc
 
-        # Name of the directory and file where the features will be saved
-        features_folder = "extracted_features/feat_extr_simple"
+        # Feature Extraction Initialization Function with only one method and only one hyperparameter
+        if k_values_frame == [-1]:
+            # Name of the directory and file where the features will be saved
+            features_folder = "extracted_features/feat_extr_simple"
 
-        # Check if the directory exists, if not, report error, data mismatch
-        if not os.path.exists(features_folder):
-            print("Error, data mismatch, features folder doesn't exist in test classifier")
+            # Check if the directory exists, if not, report error, data mismatch
+            if not os.path.exists(features_folder):
+                print("Error, data mismatch, features folder doesn't exist in test classifier")
 
-        feature_filename_target = "extracted_features_" + str(k_mfcc) + ".npy"
-        label_filename_target = "extracted_labels_" + str(k_mfcc) + ".npy"
-        feature_filename = os.path.join(features_folder, feature_filename_target)
-        label_filename = os.path.join(features_folder, label_filename_target)
+            feature_filename_target = "extracted_features_" + str(k_mfcc) + ".npy"
+            label_filename_target = "extracted_labels_" + str(k_mfcc) + ".npy"
+            feature_filename = os.path.join(features_folder, feature_filename_target)
+            label_filename = os.path.join(features_folder, label_filename_target)
 
-        # Check if the file doesn't exist
-        if os.path.exists(feature_filename):
-            features = np.load(feature_filename)
-            labels = np.load(label_filename)
+            # Check if the file doesn't exist
+            if os.path.exists(feature_filename):
+                features = np.load(feature_filename)
+                labels = np.load(label_filename)
 
-            # Train and evaluate the different classifiers outlined in training.py
-            results.append(test_classifier(features, labels, n_mfcc=n_mfcc, models_used=models_used))
+                # Train and evaluate the different classifiers outlined in training.py
+                results.append(test_classifier(features, labels, n_mfcc=n_mfcc, models_used=models_used))
+            else:
+                print("Error, data mismatch, features and labels data don't exist in features folder in test classifier")
         else:
-            print("Error, data mismatch, features and labels data don't exist in features folder in test classifier")
+            for k_frame in k_values_frame:
+                frame_size = 2 ** k_frame
+                hop_length = frame_size // 2  # 50% overlap
+
+                # Feature Extraction Initialization Function with all five methods and only two hyperparameters
+                if k_values_segment == [-1]:
+                    # Name of the directory and file where the features will be saved
+                    features_folder = "extracted_features/feat_extr"
+
+                    # Check if the directory exists, if not, report error, data mismatch
+                    if not os.path.exists(features_folder):
+                        print("Error, data mismatch, features folder doesn't exist in test classifier")
+
+                    feature_filename_target = "extracted_features_" + str(k_mfcc) + str(k_frame) + ".npy"
+                    label_filename_target = "extracted_labels_" + str(k_mfcc) + str(k_frame) + ".npy"
+                    feature_filename = os.path.join(features_folder, feature_filename_target)
+                    label_filename = os.path.join(features_folder, label_filename_target)
+
+                    # Check if the file doesn't exist
+                    if os.path.exists(feature_filename):
+                        features = np.load(feature_filename)
+                        labels = np.load(label_filename)
+
+                        # Train and evaluate the different classifiers outlined in training.py
+                        results.append(test_classifier(features, labels, n_mfcc=n_mfcc, frame_size=frame_size, models_used=models_used))
+                    else:
+                        print(
+                            "Error, data mismatch, features and labels data don't exist in features folder in test classifier")
+                else:
+                    for k_segment in k_values_segment:
+                        n_segments = 10 * k_segment
+
+                        # Feature Extraction Initialization Function with all five methods and all three hyperparameters
+
+                        # Name of the directory and file where the features will be saved
+                        features_folder = "extracted_features/feat_extr_with_segm"
+
+                        # Check if the directory exists, if not, report error, data mismatch
+                        if not os.path.exists(features_folder):
+                            print("Error, data mismatch, features folder doesn't exist in test classifier")
+
+                        feature_filename_target = "extracted_features_" + str(k_mfcc) + str(k_frame) + str(k_segment) + ".npy"
+                        label_filename_target = "extracted_labels_" + str(k_mfcc) + str(k_frame) + str(k_segment) + ".npy"
+                        feature_filename = os.path.join(features_folder, feature_filename_target)
+                        label_filename = os.path.join(features_folder, label_filename_target)
+
+                        # Check if the file doesn't exist
+                        if os.path.exists(feature_filename):
+                            features = np.load(feature_filename)
+                            labels = np.load(label_filename)
+
+                            # Train and evaluate the different classifiers outlined in training.py
+                            results.append(test_classifier(features, labels, n_mfcc=n_mfcc, frame_size=frame_size, n_segments=n_segments, models_used=models_used))
+                        else:
+                            print(
+                                "Error, data mismatch, features and labels data don't exist in features folder in test classifier")
 
     # After the loop you can convert results to a DataFrame and analyze it
     results_df = pd.DataFrame(results)
@@ -189,5 +343,3 @@ def test_display(results_df):
 
 results_df = test_modular(data_dir)
 test_display(results_df)
-
-# TODO add support for more feature extraction hyperparameters
